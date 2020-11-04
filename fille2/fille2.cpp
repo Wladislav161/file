0ff057ecdb2e78786dc559824f3923e29ae0b8c8
Wladislav161 <corvinm@mail.ru>
2020-11-04 13:50:52 +0300

    вариант как использовать Private

---
+++ fille2/fille2.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class Class
+{
+public:
+    int a;
+    void PrintPub()
+    {
+        cout << a << endl;
+    }
+
+    void PrintPubPriv(int b)
+    {
+        PrintPriv(b);
+    }
+
+private:
+    int b;
+    void PrintPriv(int b)
+    {
+        cout << b << endl;
+    }
+
+};
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    Class Inic;
+    Inic.a=15;
+    Inic.PrintPub();
+
+    Inic.PrintPubPriv(20);
+}
